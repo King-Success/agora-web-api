c211525f51ee529ce59b467dcfcd3a0adfc517ca
King-Success <okonkwoak@gmail.com>
2018-04-16 01:30:30 +0100

    Reverted back to original code

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,4 +24,4 @@ Route::get('articles','ArticleController@index');
 Route::get('articles/{article}','ArticleController@show');
 Route::post('articles','ArticleController@store');
 Route::put('articles/{article}','ArticleController@update');
-//Route::delete('articles{article}','ArticleController@delete');
+Route::delete('articles{article}','ArticleController@delete');
